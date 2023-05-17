109876cefcbec1643db09ad350d2fbdc26bafa05
joaovictor3g <jvdias79797@gmail.com>
2023-05-17 20:47:58 -0300

    feat: configure app mobile

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { styled } from "nativewind";
+import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
+import { useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
+import { useRouter } from "expo-router";
 
 import {
   useFonts,
@@ -8,20 +13,58 @@ import {
 } from "@expo-google-fonts/roboto";
 import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
 
-import blurBg from "./src/assets/bg-blur.png";
-import Stripes from "./src/assets/stripes.svg";
-import NLWLogo from "./src/assets/nlw-spacetime-logo.svg";
-
-import { styled } from "nativewind";
+import blurBg from "../src/assets/bg-blur.png";
+import Stripes from "../src/assets/stripes.svg";
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
+import { api } from "../src/lib/api";
 
 const StyledStripes = styled(Stripes);
 
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/da31d2120d4c60c05dac",
+};
+
 export default function App() {
+  const router = useRouter();
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  const [, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: "da31d2120d4c60c05dac",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nlwspacetime",
+      }),
+    },
+    discovery
+  );
+
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post("/register", {
+      code,
+    });
+
+    const { token } = response.data;
+
+    SecureStore.setItemAsync("nlwspacetime-token", token);
+
+    router.push("/memories");
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+      handleGithubOAuthCode(code);
+    }
+  }, [response]);
 
   if (!hasLoadedFonts) return null;
 
@@ -49,6 +92,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
